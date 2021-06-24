b723ddefcfafb2c532709e77d8625edca545aade
MJTMace <17456158+MJTMace@users.noreply.github.com>
2021-06-24 17:09:34 +0100

    Fix bug in generator, addresses code review comments

--- fibonacci.py
+++ fibonacci.py
@@ -11,7 +11,7 @@ def fibonacci(n):
 
 def fib_gen(n):
     for i in range(n):
-        yield fibonacci(n) # this should be i (purposeful mistake for code review)
+        yield fibonacci(i) # this should be i (purposeful mistake for code review)
 
 if __name__=='__main__':
     print(fibonacci(35))
